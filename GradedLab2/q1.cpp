796126ad4964e60a09720b78b253fd4edd750a21
Sujith <ps335@snu.edu.in>
2023-10-14 11:11:39 +0530

    edit ignore

--- GradedLab2/q1.cpp
+++ GradedLab2/q1.cpp
@@ -14,41 +14,33 @@
 using namespace std;
 
 // using radix sort
-void sort_bucket(vector<string> &vs)
+void sort_bucket(vector<string> &vs,int max_len)
 {
     int n = vs.size();
-    int max_len = 0;
-    for (int i = 0; i < n; i++)
-        max_len = max(max_len, (int)vs[i].size());
 
+    // from each index of string starting from 0 and going till max_len
+    // for each index we compare the characters and sort the strings
+    // according to the characters at that index with the help of inbuilt sort function
+    // its an balanced sort
     for (int i = 0; i < max_len; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            if (vs[j].size() <= i)
-                continue;
-            int index = vs[j][i] - 'a';
-            vector<string> temp;
-            temp.push_back(vs[j]);
-            for (int k = j + 1; k < n; k++)
-            {
-                if (vs[k].size() <= i)
-                    continue;
-                if (vs[k][i] == vs[j][i])
+            for (int k = j + 1; k < n; k++){
+                if (vs[j][i] > vs[k][i])
+                    swap(vs[j], vs[k]);
+                else if (vs[j][i] == vs[k][i])
                 {
-                    temp.push_back(vs[k]);
-                    vs.erase(vs.begin() + k);
-                    n--;
-                    k--;
+                    if (vs[j].size() > vs[k].size())
+                        swap(vs[j], vs[k]);
                 }
-            }
-            sort(temp.begin(), temp.end());
-            for (int k = 0; k < temp.size(); k++)
-            {
-                vs[j + k] = temp[k];
             }
         }
     }
+
+    
+
+
 }
 
 int main()
@@ -59,17 +51,25 @@ int main()
     cout << "Enter Number of Strings to sort : ";
     cin >> n;
     vector<string> vs(n, "");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++){
         cin >> vs[i];
+
+        // converting to lowercase
+        transform(vs[i].begin(), vs[i].end(), vs[i].begin(), ::tolower);
+        cout << vs[i] << endl;
+    }
 
     // segregate into buckets
     vector<vector<string>> bucket(26, vector<string>());
-    for (int i = 0; i < n; i++)
-        bucket[vs[i][0] - 'a'].push_back(vs[i]);
+    vector<int> max_len(26, 0);
+    for (int i = 0; i < n; i++){
+        max_len[vs[i][0] - 'a'] = max(max_len[vs[i][0] - 'a'], (int)vs[i].size());
+        bucket[vs[i][0] - 'a'].push_back(vs[i]);}
 
     // sorting each bucket
     for (int i = 0; i < 26; i++)
-        sort_bucket(bucket[i]);
+        if (bucket[i].size() > 0)
+            sort_bucket(bucket[i], max_len[i]);
 
     // printing the sorted order of strings
     for (int i = 0; i < 26; i++)
