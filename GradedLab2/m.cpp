a87bcde104947330f62d75f1c0dfbe7162c564d2
Sujith <ps335@snu.edu.in>
2023-10-19 23:20:47 +0530

    format code

--- GradedLab2/m.cpp
+++ GradedLab2/m.cpp
@@ -1,14 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-typedef struct tuples{
+typedef struct tuples
+{
     int index;
     int word;
     char letter;
-}tuples;
+} tuples;
 
-void sort_by_letter(vector<tuples> &v){
+void sort_by_letter(vector<tuples> &v)
+{
     vector<vector<tuples>> m(26);
     for (int i = 0; i < v.size(); i++)
     {
@@ -22,10 +24,10 @@ void sort_by_letter(vector<tuples> &v){
             v.push_back(m[i][j]);
         }
     }
-
 }
 
-void sort_by_index(vector<tuples> &v, int len){
+void sort_by_index(vector<tuples> &v, int len)
+{
     vector<vector<tuples>> m(len);
     for (int i = 0; i < v.size(); i++)
     {
@@ -41,11 +43,12 @@ void sort_by_index(vector<tuples> &v, int len){
     }
 }
 
-void arrange_words_by_length(int max_len, vector<string> &words){
+void arrange_words_by_length(int max_len, vector<string> &words)
+{
     vector<vector<string>> m(max_len);
     for (int i = 0; i < words.size(); i++)
     {
-        m[words[i].length()-1].push_back(words[i]);
+        m[words[i].length() - 1].push_back(words[i]);
     }
     words.clear();
     for (int i = 0; i < max_len; i++)
@@ -57,20 +60,22 @@ void arrange_words_by_length(int max_len, vector<string> &words){
     }
 }
 
-int main(){
+int main()
+{
 
-    int n, len=0;
-    cout<<"Enter the number of words: ";
-    cin>>n;
+    int n, len = 0;
+    cout << "Enter the number of words: ";
+    cin >> n;
 
     vector<tuples> v;
     vector<string> words;
     for (int i = 0; i < n; i++)
     {
         string s;
-        cin>>s;
+        cin >> s;
         words.push_back(s);
-        if(len < s.length()){
+        if (len < s.length())
+        {
             len = s.length();
         }
     }
@@ -78,6 +83,7 @@ int main(){
     // sort the vector of tuples by length
     arrange_words_by_length(len, words);
 
+    // create a vector of tuples
     for (int i = 0; i < words.size(); i++)
     {
         for (int j = 0; j < words[i].length(); j++)
@@ -90,34 +96,31 @@ int main(){
         }
     }
 
-    cout<<"Tuples before any sorting: \n";
+    cout << "Tuples before any sorting: \n";
     for (int i = 0; i < v.size(); i++)
     {
-        cout<<v[i].index<<" "<<v[i].letter<<" "<<words[v[i].word]<<endl;
+        cout << v[i].index << " " << v[i].letter << " " << words[v[i].word] << endl;
     }
 
     // sort the vector of tuples by letter
     sort_by_letter(v);
-    cout<<"Tuples after sorting the characters: \n";
+    cout << "Tuples after sorting the characters: \n";
     for (int i = 0; i < v.size(); i++)
     {
-        cout<<v[i].index<<" "<<v[i].letter<<" "<<words[v[i].word]<<endl;
+        cout << v[i].index << " " << v[i].letter << " " << words[v[i].word] << endl;
     }
 
     // sort the vector of tuples by index
     sort_by_index(v, len);
-    cout<<"Tuples after sorting the indexes: \n";
+    cout << "Tuples after sorting the indexes: \n";
     for (int i = 0; i < v.size(); i++)
     {
-        cout<<v[i].index<<" "<<v[i].letter<<" "<<words[v[i].word]<<endl;
+        cout << v[i].index << " " << v[i].letter << " " << words[v[i].word] << endl;
     }
-    
 
-    cout<<"The sorted order is: \n";
+    cout << "The sorted order is: \n";
     for (int i = 0; i < n; i++)
     {
-        cout<<words[v[i].word]<<"\n";
+        cout << words[v[i].word] << "\n";
     }
-
 }
-
